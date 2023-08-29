05a6b52ceaf017350cd6b792567c61ccc0e2778a
CodeBytes101 <roshanleharwani@gmail.com>
2023-08-29 17:37:12 +0530

    commit

--- main.py
+++ main.py
@@ -2,20 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 import json
 from flask import Flask,render_template,request
+import random
 
 with open("dailyword.json", "r") as file:
     data = json.load(file)
 
+def thought():
+    with open('quotes.json', 'r',encoding='utf-8') as json_file:
+        data = json.load(json_file)
+        quote = random.choice(list(data.keys()))
+        meaning = data[quote]
+        return quote,meaning
+
+
 def define(word):
-    response = requests.get("https://api.dictionaryapi.dev/api/v2/entries/en/"+word)
+    response = requests.get("https://www.merriam-webster.com/dictionary/"+word)
 
-    if response.status_code == 200:
-        data = response.json()[0]
-        definition = data['meanings'][0]['definitions'][0]['definition']
-        synonyms = data['meanings'][0]['definitions'][0]['synonyms']
-        antonyms = data['meanings'][0]['definitions'][0]['antonyms']
-        return definition ,synonyms,antonyms
-
+    if response.status_code == requests.codes.ok:
+        soup  = BeautifulSoup(response.content, "html.parser")
+        data = soup.find('div',class_ = 'sense-content w-100')
+        definition = data.text
+        return definition
+    else:
+        return None
 
 def get_word_of_the_day():
     response = requests.get("https://www.merriam-webster.com/word-of-the-day")
@@ -38,25 +47,29 @@ def word_inserter(data):
         with open("dailyword.json",'w') as file:
             json.dump(daily_word,file)
             
-word_inserter(data)
-
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html',data = data)
+    word_inserter(data)
+    quote,meaning = thought()
+    return render_template('index.html',data = data,quote=quote,meaning=meaning)
 
 
 @app.route('/search',methods = ["GET","POST"])
 def search_word():
     if request.method == 'POST':
         word = request.form.get('word')
-        definition ,synonyms ,antonyms = define(word)
-        return render_template('result.html',word= word , definition = definition,synonyms = synonyms,antonyms=antonyms)
+        definition = define(word)
+        if definition != None:
+            return render_template('result.html',word= word,definition = definition)
+        else:
+            return render_template('error.html')
     else:
         return render_template('search.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
